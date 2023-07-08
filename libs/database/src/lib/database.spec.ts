3903bafe304a03375b4087194575bcc97bff07a2
ajithcr <craviajith@gmail.com>
2023-07-08 22:14:39 +0530

    Database connection

--- libs/database/src/lib/database.spec.ts
+++ libs/database/src/lib/database.spec.ts
@@ -1,7 +1,7 @@
 import { database } from './database';
 
 describe('database', () => {
-  it('should work', () => {
-    expect(database()).toEqual('database');
+  it('should work', async () => {
+    expect(await database.authenticate()).toBe(void 0);
   });
 });
--- libs/database/src/lib/database.ts
+++ libs/database/src/lib/database.ts
@@ -1,3 +1,11 @@
-export function database(): string {
-  return 'database';
-}
+import { Sequelize } from "sequelize-typescript";
+
+export const database = new Sequelize({
+  host: process.env['DB_HOST'],
+  database: process.env['DB_NAME'],
+  port: +process.env['DB_PORT']!,
+  dialect: 'postgres',
+  username: process.env['DB_USER'],
+  password: process.env['DB_PASSWORD'],
+  models: [__dirname + '/**/*.model.ts']
+})
