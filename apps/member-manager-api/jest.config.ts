1fa47b7dac73e08540f11e2e909eba3a6623cc1f
ajithcr <craviajith@gmail.com>
2023-07-08 20:28:30 +0530

    Base setup

---
+++ apps/member-manager-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'member-manager-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/member-manager-api',
+};
